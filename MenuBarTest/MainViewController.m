da56fc4f1e6543ce3f1a7f95f1afea31a1a57dd4
Zacharias Pasternack <zacharias.pasternack@gmail.com>
2015-08-05 23:02:20 -0700

    Initial commit

---
+++ MenuBarTest/MainViewController.m
@@ -0,0 +1,120 @@
+//
+//  MainViewController.m
+//  MenuBarTest
+//
+//  Created by Zacharias Pasternack on 6/19/15.
+//  Copyright (c) 2015 Fat Apps, LLC. All rights reserved.
+//
+
+@import QuartzCore;
+#import "MainViewController.h"
+
+
+@interface MainViewController ()
+
+@property( strong, nonatomic ) NSPopover* popover;
+@property( assign, nonatomic ) BOOL showingIcon;
+@property( weak, nonatomic ) IBOutlet NSTextField* titleLabel;
+@property( weak, nonatomic ) IBOutlet NSTextField* descriptionLabel;
+@property( weak, nonatomic ) IBOutlet NSImageView* imageView;
+
+@end
+
+
+@implementation MainViewController
+
+
+- (void) toggleWindow:(id)sender
+{
+	if( !self.popover.shown ) {
+		[self showPopover:sender];
+	}
+	else {
+		[self closePopover];
+	}
+}
+
+
+- (void) showPopover:(id)sender
+{
+	NSRect aRect = [sender bounds];
+	[self.popover showRelativeToRect:aRect
+							  ofView:sender
+					   preferredEdge:NSMaxYEdge];
+}
+
+
+- (void) closePopover
+{
+	[self.popover performClose:self];
+}
+
+
+- (void) viewDidLoad
+{
+	[super viewDidLoad];
+
+	self.imageView.image = [[NSWorkspace sharedWorkspace] iconForFile:NSHomeDirectory()];
+	self.showingIcon = YES;
+	
+	// HACK
+	self.titleLabel.drawsBackground = YES;
+	self.titleLabel.backgroundColor = [NSColor clearColor];
+//	self.titleLabel.textColor = [NSColor textColor];
+	self.descriptionLabel.drawsBackground = YES;
+	self.descriptionLabel.backgroundColor = [NSColor clearColor];
+	self.descriptionLabel.textColor = [NSColor textColor];
+}
+
+
+#pragma mark - IBActions
+
+
+- (IBAction) toggleIcon:(id)sender
+{
+	NSTimeInterval imageAnimationDuration = 0.5;
+	if( [[self.view.window currentEvent] modifierFlags] & NSShiftKeyMask ) {
+		imageAnimationDuration *= 10.0;
+	}
+
+	CGPoint fromPoint = self.imageView.layer.position;
+	CGPoint toPoint;
+	
+	if( self.showingIcon ) {
+		// Transition it up offscreen.
+		toPoint = CGPointMake( fromPoint.x, self.view.frame.size.height + 10 );
+		
+		self.showingIcon = NO;
+	}
+	else {
+		// Transition it back onscreen.
+		toPoint = CGPointMake( fromPoint.x, 33 );
+		
+		self.showingIcon = YES;
+	}
+	
+	CABasicAnimation* positionAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
+	positionAnimation.fromValue = [NSValue valueWithPoint:fromPoint];
+	positionAnimation.toValue = [NSValue valueWithPoint:toPoint];
+	positionAnimation.duration = imageAnimationDuration;
+	[self.imageView.layer addAnimation:positionAnimation forKey:@"position"];
+	self.imageView.layer.position = toPoint;
+}
+
+
+#pragma mark - Properties
+
+
+- (NSPopover*) popover
+{
+	if( _popover == nil ) {
+		_popover = [[NSPopover alloc] init];
+		_popover.contentViewController = self;
+		_popover.behavior = NSPopoverBehaviorTransient;
+		_popover.animates = NO;
+	}
+	return _popover;
+}
+
+
+@end
---
+++ Readme.markdown
@@ -0,0 +1,5 @@
+### What's this? ###
+
+This is simply sample code for [this stackoverflow question](http://stackoverflow.com/questions/31847828/animation-artifacts-when-nsimageview-intersects-nstextfield-in-an-nspopover). 
+
+Pay it no mind. This is not the sample code you're looking for.
